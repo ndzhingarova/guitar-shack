490b9dfa477062367ac7963bed71c1f28b68191e
Nikoleta Dzhingarova <n.dzhingarova@gmail.com>
2022-06-05 13:48:09 -0400

    removed unused import SalesHistoryTest

--- src/test/java/com/example/unittest/SalesHistoryTest.java
+++ src/test/java/com/example/unittest/SalesHistoryTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 public class SalesHistoryTest {
